9e87ec1d1b732fb389cd800028a37d86f69a179a
yhppjk <342089572@qq.com>
2023-01-26 10:24:10 +0100

    20230126

--- Cour3/carte.py
+++ Cour3/carte.py
@@ -1,1 +1,17 @@
 import numpy as np
+import random
+import matplotlib as plt
+
+world_size = 100.
+landmarks = np.array([[20,20],[20.,80],[80,20],[80,80]],float)
+
+class robot:
+    def __int__(self):
+        self.x = random.random()*world_size
+        self.y = random.random()*world_size
+        self.orientation = random.random() * 2 * np.pi
+
+    def set(self, x, y, orientation):
+        self.x = x
+        self.y = y
+        self.orientation = orientation
