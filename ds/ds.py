821732b4f91b39bd4128daf1d0d4b08b06a06ca8
yhppjk <342089572@qq.com>
2023-01-27 12:29:56 +0100

    ds

--- ds/ds.py
+++ ds/ds.py
@@ -1,6 +1,21 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-def readmap()
+def readmap():
     img = cv2.imread("map.png")
-    img = 
+    # dst =cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
+    dst = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # cv2.imshow('dst', dst)
+    # cv2.waitKey(0)
+    return dst
+
+
+map = readmap()
+print(map)
+# print(map.shape[0])
+
+
+plt.figure()
+plt.imshow(map,'gray')
+plt.show()
